7bfd1598b8de537fe4b2ca3702b11bff48443367
Gabriel Nechita <nechita.gabriel@gmail.com>
2018-07-01 12:21:45 +0300

    added contact form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'; // only import in AppModule
 import { RouterModule, Routes } from '@angular/router';
-
+import { BookmarkModule } from './bookmark/bookmark.module';
 // containers
 import { AppComponent } from './containers/app/app.component';
 
@@ -10,10 +10,11 @@ import { NavComponent } from './components/nav/nav.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
 
 const ROUTES: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'bookmarks', loadChildren: './bookmark/bookmark.module#BookmarkModule' }, // lazy loading
+  { path: 'bookmarks', loadChildren: './bookmark/bookmark.module#BookmarkModule' }, // lazy loading, disable if you don't want to use it
   { path: 'home', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
   { path: '**', component: NotFoundComponent } // 404 not found
@@ -22,14 +23,16 @@ const ROUTES: Routes = [
 @NgModule({
   imports: [
     BrowserModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES),
+    // BookmarkModule // if you want to not use lazy loading
   ],
   declarations: [
     AppComponent,
     NavComponent,
     NotFoundComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    ContactFormComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
